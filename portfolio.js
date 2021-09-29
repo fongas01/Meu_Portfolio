51a2eb4015e48af9b91e923ac8f16a6e8841c802
fongas01 <nando.fongas@gmail.com>
2021-09-29 11:43:34 -0300

    JS - Fazer funcionar o botão whatsApp

---
+++ portfolio.js
@@ -0,0 +1,10 @@
+//let email;
+
+function whatsApp() {
+
+    let mensagem;
+
+    mensagem = "Olá, me chamo Fernando, satisfação em ter-te aqui."
+
+    window.open("http://wa.me/+5521993817779?text=" + "  " + mensagem);
+}
